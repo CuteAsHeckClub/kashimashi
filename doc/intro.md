ce3be314f50d14f7c7b04d09fbd3039dd9137725
Liss McCabe <liss@eristiccode.com>
2019-08-12 14:59:59 -0700

    :sparkles: Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to kashimashi
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/kashimashi/core_test.clj
@@ -0,0 +1,7 @@
+(ns kashimashi.core-test
+  (:require [clojure.test :refer :all]
+            [kashimashi.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
